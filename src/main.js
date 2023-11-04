b2ed5affcfc502633f334cf16f3aa9d8cd15b912
songlongfei1 <songlongfei1@360.cn>
2023-11-04 17:27:38 +0800

    remove router

--- src/main.js
+++ src/main.js
@@ -5,12 +5,12 @@ import { createPinia } from 'pinia'
 import './utils/lib-flexible';
 
 import App from './App.vue'
-import router from './router'
+// import router from './router'
 
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
+// app.use(router)
 
 app.mount('#app')
